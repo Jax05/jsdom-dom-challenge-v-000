855624512337b77fb2eca007a5881be84d8c541a
Carley Tripp <carley.tripp@outlook.com>
2020-03-28 02:57:28 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
       heart.disabled = false;
       plus.disabled = false;
       minus.disabled = false;
-      
+
       startInterval;
     }
   };
