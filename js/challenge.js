c96074d270651d1c757bf30d92edecfaa9ccefc5
Carley Tripp <carley.tripp@outlook.com>
2020-03-28 03:20:02 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let heart = document.getElementById("heart");
   let pause = document.getElementById("pause");
   let submit = document.getElementById("submit");
-  let likes = document.getElementsByClassName("likes");
+  let likes = document.getElementsByClassName("likes")[0];
 
   minus.addEventListener("click", decrement);
   plus.addEventListener("click", increment);
