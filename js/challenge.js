0d49a3afa02fb126793d9e76520eee5e07735d0a
Carley Tripp <carley.tripp@outlook.com>
2020-03-28 02:50:21 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -39,14 +39,14 @@ document.addEventListener("DOMContentLoaded", () => {
   function pauseButtonClicked() {
     if (pause.innerText == 'pause') {
       pause.innerText = 'resume';
-      
+
       document.getElementById("submit").disabled = true;
       document.getElementById("heart").disabled = true;
       document.getElementById("plus").disabled = true;
       document.getElementById("minus").disabled = true;
     } else {
       pause.innerText = 'pause';
-      
+
       document.getElementById("submit").disabled = false;
       document.getElementById("heart").disabled = false;
       document.getElementById("plus").disabled = false;
